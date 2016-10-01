975341cbcddfd0758701de1d93a9529c0fc10575
Pedro Mayorga <ppmayorga80@gmail.com>
2016-10-01 03:41:51 -0500

    redirect for all

--- app/Http/Controllers/UrlTransformController.php
+++ app/Http/Controllers/UrlTransformController.php
@@ -11,7 +11,8 @@ class UrlTransformController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
+        $this->middleware('auth', ['except' => 'index']);
     }
 
     public function index($key=-1)
